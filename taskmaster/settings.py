d86c827e26cfcc12e958bb6ab9d0f2cb05e0ebd7
Mahanthi Leela Krishna Prasad <mahanthileelakrishnaprasad@gmail.com>
2026-06-26 01:03:58 +0530

    cors

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -117,6 +117,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://mythings-xnic.onrender.com',
 ]
 CORS_ALLOW_CREDENTIALS = True
 # Add your production frontend URL here once deployed, e.g.:
